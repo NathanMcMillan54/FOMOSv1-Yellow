ec8076de600cc55f23a7e774f7c811b118e72730
Nathan McMillan <nathanmcmillan54@gmail.com>
2020-07-22 19:40:45 -0400

    Added time, added css to homeScreen.html.

--- jsUi/homeScreen.js
+++ jsUi/homeScreen.js
@@ -6,3 +6,10 @@ var settingsBtn = document.getElementById('settingsBtn');
 settingsBtn.addEventListener('click', function () {
     main.settingsWindow();
 })
+
+var time = setInterval(time, 1000);
+function time() {
+    var time = new Date();
+    document.getElementById("time").innerHTML = time;
+}
+
