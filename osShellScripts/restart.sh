6a99063a8138477bf2b250f9ab85c4d9651adb96
Nathan McMillan <nathanmcmillan54@gmail.com>
2020-07-23 14:11:00 -0400

    The if statment to run shutdown.sh and restart.sh dosen't work so it was removed, the FOMOS/Linux command restart doesn't exist apperantly you have to use the reboot command.

--- osShellScripts/restart.sh
+++ osShellScripts/restart.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Restarting..."
-    restart
-else
-    echo "Restart doesn't work on this OS"
-fi
+reboot
--- osShellScripts/shutdown.sh
+++ osShellScripts/shutdown.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Shutting down..."
-    shutdown -P now
-else
-    echo "Shutdown doesn't work on this OS"
-fi
+shutdown -P now
