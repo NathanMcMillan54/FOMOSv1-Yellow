e7bedf40f4dfa3f1b6ff89cf40d791404e069c0b
Nathan McMillan <nathanmcmillan54@gmail.com>
2020-07-23 09:07:25 -0400

    Connecting to wifi should work.

--- osJavaScript/wifi.js
+++ osJavaScript/wifi.js
@@ -10,4 +10,23 @@ function localSsid() {
     })
 }
 
+
+function connectToWifi() {
+    event.preventDefault();
+    var ssid = document.getElementById("ssid");
+    console.log(ssid.value);
+
+    event.preventDefault();
+    var password = document.getElementById("password");
+    console.log("Password was entered");
+
+    var connectToWifi = exec('sudo sh osShellScripts/connectWifi.sh ' + ssid.value + ' ' + password.value, (err, stdout, stderr) => {
+        if (err) {
+            console.log(`exec error: ${err}`);
+            return;
+        }
+        document.getElementById("connectToWifi").innerHTML = `${stdout}`
+    })
+}
+
 localSsid();
