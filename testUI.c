52aaa6cdcece0464129d3cfc104d00d1d37d1a59
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-08-06 15:30:33 -0400

    Started making a C UI to replace the Js UI, C will make FOMOS run faster.

--- testUI.c
+++ testUI.c
@@ -5,7 +5,7 @@ int main (int argc, char **argv) {
     printf("Starting FOMOS...");
     gtk_init (&argc, &argv);
     GtkWidget *w = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title (GTK_WINDOW (w), "Hello World!");
+    gtk_window_set_title (GTK_WINDOW (w), "FOMOS");
     gtk_window_set_default_size (GTK_WINDOW (w), 200, 200);
     g_signal_connect (w, "destroy", G_CALLBACK (gtk_main_quit), NULL);
     gtk_widget_show (w);
