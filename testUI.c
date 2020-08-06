9f7cc388a0ded9e089ab511019efb40dbc7a2844
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-08-06 15:43:49 -0400

    FOMOS C fullscreens now.

--- testUI.c
+++ testUI.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 int main (int argc, char **argv) {
-    printf("Starting FOMOS...");
+    printf("Starting FOMOS... \n");
     gtk_init (&argc, &argv);
     GtkWidget *w = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title (GTK_WINDOW (w), "FOMOS");
-    gtk_window_set_default_size (GTK_WINDOW (w), 200, 200);
+    gtk_window_fullscreen(GTK_WINDOW(w));
     g_signal_connect (w, "destroy", G_CALLBACK (gtk_main_quit), NULL);
     gtk_widget_show (w);
     gtk_main ();
