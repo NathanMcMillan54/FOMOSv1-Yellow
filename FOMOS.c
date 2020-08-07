08d9c5b82be6610731e21d985de801a4e18e3a3a
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-08-07 13:08:27 -0400

    Added exit button to settings, started working on time on homescreen.

--- FOMOS.c
+++ FOMOS.c
@@ -1,19 +1,35 @@
 #include <gtk/gtk.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
-void buttonSettings (GtkButton *button, GtkLabel *sum) {
+void buttonSettings (GtkButton *button) {
     system("sh settings.sh");
 }
 
-void buttonGoogle (GtkButton *button, GtkLabel *sum) {
+void buttonGoogle (GtkButton *button) {
     system("xdg-open http://www.google.com");
+}
 
+int showTime() {
+    time_t timer;
+    char buffer[26];
+    struct tm* tm_info;
+
+    timer = time(NULL);
+    tm_info = localtime(&timer);
+
+    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+    puts(buffer);
+    sleep(1);
 }
 
 int main(int argc, char **argv) {
+    showTime();
     char google[50] = "Google";
     char settings[50] = "Settings";
+
     GtkWidget *window;
     GtkWidget *settingsButton;
     GtkWidget *googleButton;
@@ -26,9 +42,9 @@ int main(int argc, char **argv) {
     googleButton = gtk_button_new_with_label ("Google");
 
     gtk_window_fullscreen(GTK_WINDOW(window));
-    gtk_grid_set_row_spacing       (GTK_GRID(grid), 1);
-    gtk_grid_set_column_spacing    (GTK_GRID(grid), 1);
-    gtk_container_add              (GTK_CONTAINER(window), grid);
+    gtk_grid_set_row_spacing (GTK_GRID(grid), 1);
+    gtk_grid_set_column_spacing (GTK_GRID(grid), 1);
+    gtk_container_add (GTK_CONTAINER(window), grid);
 
     gtk_grid_attach (GTK_GRID(grid), settingsButton, 0, 0, 1, 1);
     gtk_grid_attach (GTK_GRID(grid), googleButton, 1, 0, 1, 1);
@@ -38,8 +54,6 @@ int main(int argc, char **argv) {
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(G_OBJECT(settingsButton), "clicked", G_CALLBACK(buttonSettings), settings);
     g_signal_connect(G_OBJECT(googleButton), "clicked", G_CALLBACK(buttonGoogle), google);
-
-
 
     gtk_main();
 
--- settings.c
+++ settings.c
@@ -2,15 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
+void buttonExit() {
+    exit(0);
+}
+
 int main(int argc, char **argv) {
     GtkWidget *window;
     GtkWidget *grid;
     GtkWidget *text;
+    GtkWidget *exitButton;
 
     gtk_init(&argc, &argv);
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     text = gtk_label_new("Settings");
     grid = gtk_grid_new();
+    exitButton = gtk_button_new_with_label ("Exit");
 
     gtk_window_fullscreen(GTK_WINDOW(window));
     gtk_label_set_selectable (GTK_LABEL(text), TRUE);
@@ -18,11 +24,13 @@ int main(int argc, char **argv) {
     gtk_grid_set_column_spacing (GTK_GRID(grid), 4);
     gtk_container_add (GTK_CONTAINER(window), grid);
 
-    gtk_grid_attach (GTK_GRID(grid), text, 0, 2, 2, 1);
+    gtk_grid_attach (GTK_GRID(grid), text, 0, 0, 1, 1);
+    gtk_grid_attach (GTK_GRID(grid), exitButton, 0, 2, 2, 1);
 
     gtk_widget_show_all (window);
 
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(G_OBJECT(exitButton), "clicked", G_CALLBACK(buttonExit), buttonExit);
 
     gtk_main();
 
