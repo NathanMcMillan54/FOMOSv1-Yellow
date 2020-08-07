3c6a9f4a80b1b3c6d3ad3bfb937d0c233e391267
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-08-06 23:55:01 -0400

    FOMOS might be made with GTK instead of ElectronJs.

--- FOMOS.c
+++ FOMOS.c
@@ -1,25 +1,45 @@
 #include <gtk/gtk.h>
-#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-static void activate (GtkApplication* app, gpointer user_data) {
-    GtkWidget *window;
-
-    window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "FOMOS");
-    gtk_window_fullscreen (GTK_WINDOW(window));
-    gtk_widget_show_all (window);
+void buttonSettings (GtkButton *button, GtkLabel *sum) {
+    system("sh settings.sh");
 }
 
-int main (int argc, char **argv) {
-    GtkApplication *app;
-    int status;
+void buttonGoogle (GtkButton *button, GtkLabel *sum) {
+    system("xdg-open http://www.google.com");
 
-    printf("Starting FOMOS");
+}
 
-    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
+int main(int argc, char **argv) {
+    char google[50] = "Google";
+    char settings[50] = "Settings";
+    GtkWidget *window;
+    GtkWidget *settingsButton;
+    GtkWidget *googleButton;
+    GtkWidget *grid;
 
-    return status;
+    gtk_init (&argc,&argv);
+    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    grid = gtk_grid_new ();
+    settingsButton = gtk_button_new_with_label ("Settings");
+    googleButton = gtk_button_new_with_label ("Google");
+
+    gtk_window_fullscreen(GTK_WINDOW(window));
+    gtk_grid_set_row_spacing       (GTK_GRID(grid), 1);
+    gtk_grid_set_column_spacing    (GTK_GRID(grid), 1);
+    gtk_container_add              (GTK_CONTAINER(window), grid);
+
+    gtk_grid_attach (GTK_GRID(grid), settingsButton, 0, 0, 1, 1);
+    gtk_grid_attach (GTK_GRID(grid), googleButton, 1, 0, 1, 1);
+
+    gtk_widget_show_all (window);
+
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(G_OBJECT(settingsButton), "clicked", G_CALLBACK(buttonSettings), settings);
+    g_signal_connect(G_OBJECT(googleButton), "clicked", G_CALLBACK(buttonGoogle), google);
+
+    gtk_main();
+
+    return 0;
 }
