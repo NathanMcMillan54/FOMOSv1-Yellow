9f60233e7d7c5f16bded7cd7a84588757f69cb7b
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-08-12 11:10:10 -0400

    Added installCFOMOSRequirements.sh in CFOMOS/ for people using CFOMOS.

---
+++ CFOMOS/installCFOMOSRequirements.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Installing GTK..."
+sudo apt-get install libgtk-3-dev
+
+echo "Installing Network-Manager..."
+sudo apt-get install network-manager
+
+echo "Installing Matchbox-Keyboard"
+sudo apt-get install network-manager
